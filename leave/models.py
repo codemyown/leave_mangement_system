236777bd19e1e6e9d845f69c144926f94e5011da
codemyown <ajaypawar6225@gmail.com>
2025-10-04 18:45:33 +0530

    feat: create custom User model with roles and department

--- leave/models.py
+++ leave/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    is_employee = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    department = models.CharField(max_length=100, blank=True, null=True)
