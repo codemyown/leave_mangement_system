dcd4ef2758f38e0a0b7ce7f3fb7e45127be49ff2
codemyown <ajaypawar6225@gmail.com>
2025-10-04 19:51:50 +0530

    feat: add URL for manager dashboard view

--- leave/urls.py
+++ leave/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
    path('dashboard/', views.dashboard_view, name='dashboard'),
 
    # Employee-specific views
-   
+
    path('apply-leave/', views.apply_leave_view, name='apply_leave'),
    path('leave-history/', views.leave_history_view, name='leave_history'),
    path('upcoming-holidays/', views.holiday_calendar_view, name='upcoming_holidays'),
    path('leave-history/download/', views.download_leave_history_pdf, name='download_leave_history_pdf'),
 
    # Manager-specific views can be added here
+   path('manager-dashboard/', views.manager_dashboard_view, name='manager_dashboard'),
 ]
