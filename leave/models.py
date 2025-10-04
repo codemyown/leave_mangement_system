c45f0af2f5b4ee2e2d3d2138c0dee0205b0e4061
codemyown <ajaypawar6225@gmail.com>
2025-10-04 19:01:35 +0530

    docs: add docstring to custom User model

--- leave/models.py
+++ leave/models.py
@@ -2,6 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    """
+    Custom User model extending AbstractUser.
+
+    Attributes:
+        is_employee (bool): Flag to indicate if the user is an employee.
+        is_manager (bool): Flag to indicate if the user is a manager.
+        department (str): Optional department name for the user.
+    """
     is_employee = models.BooleanField(default=False)
     is_manager = models.BooleanField(default=False)
     department = models.CharField(max_length=100, blank=True, null=True)
+
+
